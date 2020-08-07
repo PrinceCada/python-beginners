c43bcb9a0f87b43d485db581c367557bbd76ee82
Prince Cada <69154548+PrinceCada@users.noreply.github.com>
2020-08-07 18:49:36 +0800

    Update StarPatternPyramid.py

--- StarPatternPyramid.py
+++ StarPatternPyramid.py
@@ -1,6 +1,6 @@
 #Star Pattern Pyramid
 def pattern(n):
-    k = 2  * n - 2
+    k = 2  * n - 2                  #You can play with this, purpose is location or position only
     for i in range(0,n):            #This loop is for our outer rows
         for j in range(0,k):        #This loop is for column
             print(end = " ")
@@ -9,3 +9,16 @@ def pattern(n):
             print("* ",end="")
         print("\r")
 pattern(10)
+
+
+#Output
+                  * 
+                 * * 
+                * * * 
+               * * * * 
+              * * * * * 
+             * * * * * * 
+            * * * * * * * 
+           * * * * * * * * 
+          * * * * * * * * * 
+         * * * * * * * * * * 
